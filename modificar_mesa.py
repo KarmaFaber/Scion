fd1d4d360fd59c8034e58b83f4451d7e5b79b76a
María <52334627+KarmaFaber@users.noreply.github.com>
2021-06-07 21:38:10 +0200

    Add files via upload
    Partición del código en módulos para facilitar el trabajo en cada parte del proyecto.

---
+++ modificar_mesa.py
@@ -0,0 +1,11 @@
+#python modules----------------------------------------------------
+from tkinter import *
+from tkinter import messagebox
+import json
+#my modules-------------------------------------------------------
+
+#-----------------------------------------------------
+
+
+def modificar_mesa():
+    pass
